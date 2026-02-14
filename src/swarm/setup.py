ee8c463cecb57c3bfc441e551101528d87ab60eb
osmancevik <github.fencing841@passmail.net>
2026-02-14 12:57:39 +0300

    feat: ROS 2 bilesenleri (swarm, vision, network, gcs, msgs) olusturuldu ve CycloneDDS bagimliligi eklendi

---
+++ src/swarm/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'swarm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Yelpence',
+    maintainer_email='iletisim@yelpence.com',
+    description='Yelpence Takimi Suru Algoritmalari Paketi',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # İleride sürü kontrol düğümlerimizi buraya ekleyeceğiz. Örnek:
+            # 'formation_controller = swarm.formation_controller:main',
+        ],
+    },
+)
