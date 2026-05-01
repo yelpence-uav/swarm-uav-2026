50cd07d40b84b7807bdcab69b332e2807118c111
SeydaGulKOCAK <seyda@gmail.com>
2026-05-01 23:24:48 +0300

    fix: agent FSM eksikleri tamamlandi, PEP8 uyumu ve CI workflow duzeltildi

--- src/swarm_state_machine/swarm_state_machine/agent_fsm/agent_states.py
+++ src/swarm_state_machine/swarm_state_machine/agent_fsm/agent_states.py
@@ -4,48 +4,48 @@ from enum import IntEnum
 class AgentState(IntEnum):
     """Ajan FSM durum sabitleri. AgentStatus.msg STATE_* ile birebir eşleşir."""
 
-    UNKNOWN           = 0
-    IDLE              = 1
-    ARMING            = 2
-    ARMED             = 3
-    TAKEOFF           = 4
-    IN_SWARM          = 5
-    EXECUTING_TASK    = 6
-    DETACHED          = 7
+    UNKNOWN = 0
+    IDLE = 1
+    ARMING = 2
+    ARMED = 3
+    TAKEOFF = 4
+    IN_SWARM = 5
+    EXECUTING_TASK = 6
+    DETACHED = 7
     PRECISION_LANDING = 8
-    WAITING_REJOIN    = 9
-    REJOINING         = 10
-    RETURN_HOME       = 11
-    LANDING           = 12
-    LANDED            = 13
-    FAILSAFE          = 14
-    STANDBY           = 15
+    WAITING_REJOIN = 9
+    REJOINING = 10
+    RETURN_HOME = 11
+    LANDING = 12
+    LANDED = 13
+    FAILSAFE = 14
+    STANDBY = 15
 
 
 class AgentRole(IntEnum):
     """Ajan rol sabitleri. AgentStatus.msg ROLE_* ile birebir eşleşir."""
 
-    UNKNOWN  = 0
-    LEADER   = 1
+    UNKNOWN = 0
+    LEADER = 1
     FOLLOWER = 2
-    STANDBY  = 3
+    STANDBY = 3
     DETACHED = 4
 
 
 class FlightMode(IntEnum):
     """PX4 uçuş modu sabitleri. AgentStatus.msg FLIGHT_MODE_* ile eşleşir."""
 
-    UNKNOWN      = 0
-    MANUAL       = 1
-    ALTCTL       = 2
-    POSCTL       = 3
-    OFFBOARD     = 4
+    UNKNOWN = 0
+    MANUAL = 1
+    ALTCTL = 2
+    POSCTL = 3
+    OFFBOARD = 4
     AUTO_MISSION = 5
-    AUTO_LOITER  = 6
-    AUTO_RTL     = 7
-    AUTO_LAND    = 8
-    ACRO         = 9
-    STABILIZED   = 10
+    AUTO_LOITER = 6
+    AUTO_RTL = 7
+    AUTO_LAND = 8
+    ACRO = 9
+    STABILIZED = 10
 
 
 # State 7,8,13,14: avoidance hesabına dahil edilmez (ORCA/APF)
