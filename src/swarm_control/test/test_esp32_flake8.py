a8bb60570b5d7b9c20c5140eaae0bacdb6821f1e
Ahmet Berk Yıldız <berkyldzz0@outlook.com>
2026-07-20 12:36:54 +0300

    fix: CI test error fixed

--- src/swarm_control/test/test_esp32_flake8.py
+++ src/swarm_control/test/test_esp32_flake8.py
@@ -7,7 +7,9 @@ from ament_flake8.main import main_with_errors
 @pytest.mark.linter
 def test_flake8():
     """swarm_control/esp32_bridge altındaki Python kodu PEP 8'e uymalı."""
-    rc, errors = main_with_errors(argv=['swarm_control/esp32_bridge'])
+    rc, errors = main_with_errors(
+        argv=['swarm_control']
+    )
     assert rc == 0, (
         'Found {} code style errors / warnings:\n'.format(len(errors))
         + '\n'.join(errors)
--- src/swarm_core/test/test_flake8.py
+++ src/swarm_core/test/test_flake8.py
@@ -8,7 +8,7 @@ from ament_flake8.main import main_with_errors
 def test_flake8():
     """Check PEP 8 compliance with flake8."""
     rc, errors = main_with_errors(
-        argv=['swarm_core/formation_control']
+        argv=['swarm_core']
     )
     assert rc == 0, (
         'Found {} code style errors / warnings:\n'.format(len(errors))
