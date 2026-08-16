76cfeebc34813706ee75adaba0d332888727763b
Beyza <beyza@gmail.com>
2026-08-16 14:53:50 +0300

    chore: sim baslatici betikleri + qgc_proxy kaldirildi (COP_TEMIZLIK B2, E)

--- scripts/_run_director.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source /opt/ros/jazzy/setup.bash
-source /home/yelpence/ros2_ws/install/setup.bash
-export ROS_DOMAIN_ID=10
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI=file:///home/yelpence/ros2_ws/cyclonedds.xml
-cd /home/yelpence/ros2_ws
-exec python3 scripts/video_scenario_director.py --team 752825 \
-  --origin-lat 41.0441 --origin-lon 29.0017
